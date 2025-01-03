49bb7db7ef7fe64b247feeaf53053fc24ae8342e
kartik0603 <kartikhirapara800@gmail.com>
2025-01-03 12:40:50 +0530

    REact Task 3 Props

--- vite-project/src/components/Timer.jsx
+++ vite-project/src/components/Timer.jsx
@@ -1,68 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './Timer.css';
 
 const Timer = () => {
-
     let [flag, setFlag] = useState(true);
-    let [hour,setHour]= useState(0);
-    let [minute,setMinute]= useState(0);
-    let [second,setSecond]= useState(0);
-    let [time,setTime]= useState(0);
+    let [hour, setHour] = useState(0);
+    let [minute, setMinute] = useState(0);
+    let [second, setSecond] = useState(0);
+    let [time, setTime] = useState(0); // Time in minutes
 
     let id;
 
-
-    const  divider = () => {
-        let newTime= time/60;
-
-        setHour(Math.floor(newTime));
-        setMinute(time-(Math.floor(newTime)*60)-1);
-        setSecond(59);
-        setTime(0);
-        setFlag(!flag);
-
-        return ()=>{
+    const divider = () => {
+        if (flag && time > 0) {
+            const totalMinutes = time; // Since input is already in minutes
+            setHour(Math.floor(totalMinutes / 60));
+            setMinute(totalMinutes % 60);
+            setSecond(0); // Start with 0 seconds
+            setTime(0);
+            setFlag(false); 
+        } else {
             clearInterval(id);
-
+            setFlag(true); 
         }
-    }
+    };
 
-    useEffect(()=>{
-
-        id=setInterval((divider)=>{
-
-            if(time>0){
-                if(flag){
-                    setSecond(second-1);    
-                }
-                if(second===0){
+    useEffect(() => {
+        if (!flag) {
+            id = setInterval(() => {
+                if (second > 0) {
+                    setSecond((prev) => prev - 1);
+                } else if (minute > 0) {
                     setSecond(59);
-                    setMinute(minute-1);
-                }
-                if(minute===0){
+                    setMinute((prev) => prev - 1);
+                } else if (hour > 0) {
+                    setSecond(59);
                     setMinute(59);
-                    setHour(hour-1);
+                    setHour((prev) => prev - 1);
+                } else {
+                    clearInterval(id);
+                    setFlag(true);
                 }
-            }
+            }, 1000);
+        }
 
-            if(hour===0 && minute===0 && second===0){
-                clearInterval(id);
-            }
-        },1000);
+        return () => clearInterval(id); 
+    }, [flag, hour, minute, second]);
 
-    },[hour,minute,second]);
-    
-  return (
-    <div>
+    return (
+        <div className='timer-container'>
+            <input
+                type="number"
+                placeholder="Enter time in minutes"
+                onChange={(e) => setTime(Number(e.target.value))}
+                disabled={!flag} 
+                className='time-input'
+            />
 
-<input type='number' onChange={(e)=>{setTime(e.target.value)}}/>
+            <h1 className='timer-display'>
+                {hour}:{minute}:{second}
+            </h1>
 
-<h1>{hour}:{minute}:{second}</h1>
+            <button  className='timer-button' onClick={divider}>{flag ? 'Start' : 'Stop'}</button>
+        </div>
+    );
+};
 
-<button onClick={divider}>{flag ? 'Start' : 'Stop'}</button>
-
-
-    </div>
-  )
-}
-
-export default Timer
+export default Timer;
